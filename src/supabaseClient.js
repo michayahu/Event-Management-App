4ebe4d61dd0eaf7450a05da76b9731a5cb8f51d8
Mohamed Taha Dergham <uktaha740@gmail.com>
2026-04-28 01:58:00 +0100

    Member 4: update all pages with latest changes including auth

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://hfuyujocsrkgfezknlao.supabase.co';
+const supabaseAnonKey = 'sb_publishable_12TE-9AWZ1nig-DeLSxT8g_tOnuDjO9';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
