c13b3470ccd2595e09b2d7c04a3442764820d6ee
Mohamed Taha Dergham <uktaha740@gmail.com>
2026-04-29 13:09:02 +0100

    Fix: remove AuthProvider causing blank page crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
-import { Toaster } from 'react-hot-toast'
 import './index.css'
-import AuthProvider from './context/AuthContext.jsx'
 import Homepage from './pages/Homepage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import MyTickets from './pages/Mytickets.jsx'
@@ -13,18 +11,15 @@ import Contact from './pages/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/tickets" element={<MyTickets />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/tickets" element={<MyTickets />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/checkin" element={<CheckIn />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
